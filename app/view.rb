2f8a76bc963869a12b648f21fa43caaa3e1a155f
Dzmitrij <dzmitrij@duck.com>
2025-05-02 18:12:55 +0400

    Initial commit.

---
+++ app/view.rb
@@ -0,0 +1,9 @@
+# auto_register: false
+# frozen_string_literal: true
+
+require "hanami/view"
+
+module PawnstormAPI
+  class View < Hanami::View
+  end
+end
---
+++ config/app.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require "hanami"
+
+module PawnstormAPI
+  class App < Hanami::App
+  end
+end
